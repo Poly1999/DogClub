dd3144d5ac562f17cfb59d63caa73c58c52b0429
Poly1999 <pikalova.developer@gmail.com>
2026-04-04 13:08:16 -0400

    feat: add ContactForm - validation with yup, react-hook-form, API integration, MongoDB, email notifications via Nodemailer, toast notifications

--- frontend/src/components/ContactForm/ContactForm.js
+++ frontend/src/components/ContactForm/ContactForm.js
@@ -1,4 +1,9 @@
 import './ContactForm.css';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { createContact } from '../../api/api';
+import { toast } from 'react-toastify';
 import background from '../../images/contact-vector.png';
 import dogLeft from '../../images/contact-dog-left.png';
 import dogRight from '../../images/contact-dog-right.png';
@@ -7,7 +12,39 @@ import pet from '../../images/pet.png';
 import phone from '../../images/phone.png';
 import email from '../../images/email.png';
 
+const schema = yup.object({
+  ownerName: yup.string().required('Name is required'),
+  dogName: yup.string().required('Dog name is required'),
+  phone: yup
+    .string()
+    .required('Phone is required')
+    .matches(/^\+1\d{10}$/, 'Format: +1XXXXXXXXXX'),
+  email: yup
+    .string()
+    .required('Email is required')
+    .email('Enter a valid email'),
+});
+
 function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async data => {
+    try {
+      await createContact(data);
+      reset();
+      toast.success('Your request has been sent!');
+    } catch (error) {
+      toast.error('Something went wrong, please try again!');
+    }
+  };
+
   return (
     <section className='contact' id='contacts'>
       <img src={dogLeft} alt='dogLeft' className='dog-left' />
@@ -18,25 +55,49 @@ function ContactForm() {
           Our experts will take care of your friend
         </h2>
         <p className='contact-text'>Fill out the form so we can contact you!</p>
-        <form className='contact-form'>
+        <form className='contact-form' onSubmit={handleSubmit(onSubmit)}>
           <div className='form-input'>
             <img src={name} alt='contact-name' />
-            <input placeholder='Your name' className='contact-input' />
+            <input
+              placeholder='Your name'
+              className='contact-input'
+              {...register('ownerName')}
+            />
           </div>
+          {errors.ownerName && (
+            <p className='error'>{errors.ownerName.message}</p>
+          )}
 
           <div className='form-input'>
             <img src={pet} alt='pet-name' />
-            <input placeholder='Pet name' className='contact-input' />
+            <input
+              placeholder='Pet name'
+              className='contact-input'
+              {...register('dogName')}
+            />
           </div>
+          {errors.dogName && <p className='error'>{errors.dogName.message}</p>}
+
           <div className='form-input'>
             <img src={phone} alt='phone-num' />
-            <input placeholder='Your phone' className='contact-input' />
+            <input
+              placeholder='Your phone'
+              className='contact-input'
+              {...register('phone')}
+            />
           </div>
+          {errors.phone && <p className='error'>{errors.phone.message}</p>}
 
           <div className='form-input'>
             <img src={email} alt='email' />
-            <input placeholder='Your email' className='contact-input' />
+            <input
+              placeholder='Your email'
+              className='contact-input'
+              {...register('email')}
+            />
           </div>
+          {errors.email && <p className='error'>{errors.email.message}</p>}
+
           <button className='contact-button'>Contact us</button>
           <label className='contact-label'>
             <input type='checkbox' className='checkbox' />
